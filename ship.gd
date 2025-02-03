5a7d7e222fec6811df9321d196bb296bb85d6401
Andre Uschmann <mail@andre-uschmann.de>
2025-02-03 21:59:53 +0100

    Added start menu

--- ship.gd
+++ ship.gd
@@ -4,8 +4,14 @@ extends Area2D
 
 const LASER_SCENE = preload("res://laser.tscn")
 @onready var explosion_creator = $EffectCreator
+@onready var game_over_timer: Timer = $GameOverTimer
+
+var is_dead = false
 
 func _process(delta: float) -> void:
+	if is_dead:
+		return
+		
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
 	if Input.is_action_pressed("ui_down"):
@@ -17,6 +23,14 @@ func _process(delta: float) -> void:
 		laser.position = self.position
 
 func _on_area_entered(area: Area2D) -> void:
+	if is_dead:
+		return
 	explosion_creator.create_explosion()
-	#queue_free()
 	area.queue_free()
+	is_dead = true
+	hide()
+	game_over_timer.start()
+
+
+func _on_game_over_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://start_menu.tscn")
---
+++ start_menu.gd
@@ -0,0 +1,8 @@
+extends ColorRect
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://world.tscn")
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
